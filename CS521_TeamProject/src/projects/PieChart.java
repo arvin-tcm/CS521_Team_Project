8feb87c0946a4e419727c35aad8c363a989d2239
10010 <10010mt@npu.edu>
2016-06-28 19:31:07 -0700

    test

--- CS521_TeamProject/src/projects/PieChart.java
+++ CS521_TeamProject/src/projects/PieChart.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package cs532_mvccharts;
+package projects;
 
 import javax.swing.*;
 import java.util.*;
